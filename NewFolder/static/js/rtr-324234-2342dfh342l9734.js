6d8b35c4196dcfa5b5cc9e5b3532aa85d402de0f
Saakii <75199695+Saakii556@users.noreply.github.com>
2022-06-28 02:03:23 +0530

    Add files via upload

---
+++ NewFolder/static/js/rtr-324234-2342dfh342l9734.js
@@ -0,0 +1,314 @@
+// // document.addEventListener("DOMContentLoaded", ()=>{
+// //     // document.getElementById("23-1324dfhdfiuioyokh---").preventDefault();
+// //     document.addEventListener("click", (e)=>{
+// //     //    console.log(e);
+// //        if(e.target.id == "id-btn-0189" | e.target.id == "id-0121-011-dd-01"){
+// //            e.preventDefault();
+// //            header(e.target.href);
+// //        }
+// //     })
+// // })
+
+// function header(val){
+//       window.history.pushState(null, null, val);
+//       r3042h();
+// }
+
+// window.onhashchange = ()=>{
+//         r3042h();
+//     }
+
+// window.onload = ()=>{
+//     r3042h();
+// }
+
+// function r3042h(response){
+//         let href = window.location.href;
+//         let last_value = href.split('/').slice(-1)
+//         let rdr = document.getElementById("id-render-324234");
+//         console.log(last_value);
+//         if (last_value[0] == "you"){
+//             let d4234 = `    
+          
+
+//         `
+//             rdr.innerHTML = d4234;
+//         }
+//         else if(last_value[0] == "yourarticles"){
+            let d4236 = `    <div id="id-234234-h-b">
+            <h2><span style="padding-right: 10px;"><a href="#/yourarticles" style="text-decoration: none;">News</a></span><span><a href="#/blog">Blog</a></span></h2>
+         </div>
+     
+         <div id="id-24234-234234">
+            <div id="id-4423h543">
+                 <div id="id-r98790">
+                     <div id="id-r987901">
+                        <h2>Today</h2>
+                        <div id="id-r9879010101">
+                             <div id="id-r9879010101-01">
+                                 <h3>
+                                     This is the sample title
+                                 </h3>    
+                             </div>
+                             <div id="id-r9879010101-02">
+                                 <div id="id-r9879010101-02-01">
+                                     <p>Views <span>100</span></p>
+                                 </div> 
+                                 <div id="id-r9879010101-02-01">
+                                     <a href="#">Edit</a>
+                                 </div> 
+                                 <div id="id-r9879010101-02-01">
+                                     <a href="#">Delete</a>
+                                 </div>                           
+                             </div>
+                        </div>
+                        <div id="id-r9879010101">
+                         <div id="id-r9879010101-01">
+                             <h3>
+                                 This is the sample title
+                             </h3>    
+                         </div>
+                         <div id="id-r9879010101-02">
+                             <div id="id-r9879010101-02-01">
+                                 <p>Views <span>100</span></p>
+                             </div> 
+                             <div id="id-r9879010101-02-01">
+                                 <a href="#">Edit</a>
+                             </div> 
+                             <div id="id-r9879010101-02-01">
+                                 <a href="#">Delete</a>
+                             </div>                           
+                         </div>
+                     </div>
+                     </div>
+                     <div id="id-r987901">
+                         <h2>Previous</h2>
+                         <div id="id-r9879010101">
+                              <div id="id-r9879010101-01">
+                                  <h3>
+                                      This is the sample title
+                                  </h3>    
+                              </div>
+                              <div id="id-r9879010101-02">
+                                  <div id="id-r9879010101-02-01">
+                                      <p>Views <span>100</span></p>
+                                  </div> 
+                                  <div id="id-r9879010101-02-01">
+                                      <a href="#">Edit</a>
+                                  </div> 
+                                  <div id="id-r9879010101-02-01">
+                                      <a href="#">Delete</a>
+                                  </div>                           
+                              </div>
+                         </div>
+                         <div id="id-r9879010101">
+                          <div id="id-r9879010101-01">
+                              <h3>
+                                  This is the sample title
+                              </h3>    
+                          </div>
+                          <div id="id-r9879010101-02">
+                              <div id="id-r9879010101-02-01">
+                                  <p>Views <span>100</span></p>
+                              </div> 
+                              <div id="id-r9879010101-02-01">
+                                  <a href="#">Edit</a>
+                              </div> 
+                              <div id="id-r9879010101-02-01">
+                                  <a href="#">Delete</a>
+                              </div>                           
+                          </div>
+                      </div>
+                      </div>
+                   
+                 </div>
+                 <div id="id-r43802">
+     
+                 </div>
+             </div>
+             <div id="id-4423h544">
+                 <div id="id-44235445">
+                     <div id="id-44235445-01">
+                         <p>Total Article : <span></span></p>
+                     </div>
+                     <div id="id-44235445-01">
+                         <p>Article's Today : <span></span></p>
+                     </div>
+                     <div id="id-44235445-01">
+                         <p>Total Views : <span></span></p>
+                     </div>
+                     <div id="id-44235445-01">
+                         <p>View's Today : <span></span></p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+
+//      `
+//             rdr.innerHTML = d4236;
+//         }else if(last_value[0] == "yourincome"){
+//             let d4237 = `    <div id="id-3424-m">
+//             <div id="id-3424-m-01">
+//                 <div id="id-3424-m-01-01">
+//                     <h2>Total Views</h2>
+//                 </div>
+//                 <div id="id-3424-m-01-02">
+//                     <h2>500</h2>
+//                 </div>
+//             </div>
+//             <div id="id-3424-m-01">
+//                 <div id="id-3424-m-01-01">
+//                     <h2>Total Views (Today)</h2>
+//                 </div>
+//                 <div id="id-3424-m-01-02">
+//                     <h2>500</h2>
+//                 </div>
+//             </div>
+//             <div id="id-3424-m-01">
+//                 <div id="id-3424-m-01-01">
+//                     <h2>Total Earning</h2>
+//                 </div>
+//                 <div id="id-3424-m-01-02">
+//                     <h2>500</h2>
+//                 </div>
+//             </div>
+//             <div id="id-3424-m-01">
+//                 <div id="id-3424-m-01-01">
+//                     <h2>Today's Earning</h2>
+//                 </div>
+//                 <div id="id-3424-m-01-02">
+//                     <h2>500</h2>
+//                 </div>
+//             </div>
+//             <div id="id-3424-m-01">
+//                 <div id="id-3424-m-01-01">
+//                     <h2>Last Month Earning</h2>
+//                 </div>
+//                 <div id="id-3424-m-01-02">
+//                     <h2>500</h2>
+//                 </div>
+//             </div>
+//         </div>
+        
+
+//     `
+//             rdr.innerHTML = d4237;
+//         }else if(last_value == "blog"){
+//             let d4238 = `    <div id="id-234234-h-b">
+//             <h2><span style="padding-right: 10px;"><a href="#/yourarticles">News</a></span><span><a href="#/blog" style="text-decoration: none;">Blog</a></span></h2>
+//          </div>
+     
+//          <div id="id-24234-234234">
+//             <div id="id-4423h543">
+//                  <div id="id-r98790">
+//                      <div id="id-r987901">
+//                         <h2>Today</h2>
+//                         <div id="id-r9879010101">
+//                              <div id="id-r9879010101-01">
+//                                  <h3>
+//                                      This is the sample title
+//                                  </h3>    
+//                              </div>
+//                              <div id="id-r9879010101-02">
+//                                  <div id="id-r9879010101-02-01">
+//                                      <p>Views <span>100</span></p>
+//                                  </div> 
+//                                  <div id="id-r9879010101-02-01">
+//                                      <a href="#">Edit</a>
+//                                  </div> 
+//                                  <div id="id-r9879010101-02-01">
+//                                      <a href="#">Delete</a>
+//                                  </div>                           
+//                              </div>
+//                         </div>
+//                         <div id="id-r9879010101">
+//                          <div id="id-r9879010101-01">
+//                              <h3>
+//                                  This is the sample title
+//                              </h3>    
+//                          </div>
+//                          <div id="id-r9879010101-02">
+//                              <div id="id-r9879010101-02-01">
+//                                  <p>Views <span>100</span></p>
+//                              </div> 
+//                              <div id="id-r9879010101-02-01">
+//                                  <a href="#">Edit</a>
+//                              </div> 
+//                              <div id="id-r9879010101-02-01">
+//                                  <a href="#">Delete</a>
+//                              </div>                           
+//                          </div>
+//                      </div>
+//                      </div>
+//                      <div id="id-r987901">
+//                          <h2>Previous</h2>
+//                          <div id="id-r9879010101">
+//                               <div id="id-r9879010101-01">
+//                                   <h3>
+//                                       This is the sample title
+//                                   </h3>    
+//                               </div>
+//                               <div id="id-r9879010101-02">
+//                                   <div id="id-r9879010101-02-01">
+//                                       <p>Views <span>100</span></p>
+//                                   </div> 
+//                                   <div id="id-r9879010101-02-01">
+//                                       <a href="#">Edit</a>
+//                                   </div> 
+//                                   <div id="id-r9879010101-02-01">
+//                                       <a href="#">Delete</a>
+//                                   </div>                           
+//                               </div>
+//                          </div>
+//                          <div id="id-r9879010101">
+//                           <div id="id-r9879010101-01">
+//                               <h3>
+//                                   This is the sample title
+//                               </h3>    
+//                           </div>
+//                           <div id="id-r9879010101-02">
+//                               <div id="id-r9879010101-02-01">
+//                                   <p>Views <span>100</span></p>
+//                               </div> 
+//                               <div id="id-r9879010101-02-01">
+//                                   <a href="#">Edit</a>
+//                               </div> 
+//                               <div id="id-r9879010101-02-01">
+//                                   <a href="#">Delete</a>
+//                               </div>                           
+//                           </div>
+//                       </div>
+//                       </div>
+                   
+//                  </div>
+//                  <div id="id-r43802">
+     
+//                  </div>
+//              </div>
+//              <div id="id-4423h544">
+//                  <div id="id-44235445">
+//                      <div id="id-44235445-01">
+//                          <p>Total Article : <span></span></p>
+//                      </div>
+//                      <div id="id-44235445-01">
+//                          <p>Article's Today : <span></span></p>
+//                      </div>
+//                      <div id="id-44235445-01">
+//                          <p>Total Views : <span></span></p>
+//                      </div>
+//                      <div id="id-44235445-01">
+//                          <p>View's Today : <span></span></p>
+//                      </div>
+//                  </div>
+//              </div>
+//          </div>
+//         `
+//         rdr.innerHTML = d4238;
+//         }
+
+// }
+
+// if (window.location.href == "http://127.0.0.1:5000/data"){
+//     header("http://127.0.0.1:5000/data#/you");
+// }
+
